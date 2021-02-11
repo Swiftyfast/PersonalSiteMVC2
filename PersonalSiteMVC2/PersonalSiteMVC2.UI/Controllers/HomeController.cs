8d20176fbba8e808efb2c84b1128dc4d90d86ec5
Swiftyfast <jdavidswift@gmail.com>
2021-02-11 10:47:02 -0600

    Contact added

--- PersonalSiteMVC2/PersonalSiteMVC2.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC2/PersonalSiteMVC2.UI/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSiteMVC2.UI.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace PersonalSiteMVC2.UI.Controllers
 {
@@ -26,6 +29,53 @@ namespace PersonalSiteMVC2.UI.Controllers
         public ActionResult Resume()
         {
             return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            //Steps to send an email:
+            //1) Create a string fot he message
+            string emailBody = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response the following message: <br /><br /> {cvm.Message}";
+            //2) Create the MailMessage object - System.Net.Mail
+            MailMessage msg = new MailMessage
+            (
+                "no-reply@johndavidswift.com",
+                "jdavidswift@gmail.com",
+                "Email from johndavidswift.com",
+                emailBody
+            );
+
+            msg.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient("mail.johndavidswift.com");
+            
+            client.Credentials = new NetworkCredential("no-reply@johndavidswift.com", "LOL_NOPE");
+            client.Port = 8889;
+
+            try
+            {
+                client.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = $"Sorry, something went wrong. Error message: {ex.Message}<br />{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
---
+++ PersonalSiteMVC2/PersonalSiteMVC2.UI/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;//Added for validation
+
+namespace PersonalSiteMVC2.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required *")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is required *")]
+        [UIHint("MultilineText")]//Change from a single line textbox to a multiline
+        public string Message { get; set; }
+    }
+}
